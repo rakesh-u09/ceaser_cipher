efdd7341644007878d6e07a962dd28ed638d45b2
rakesh-u09 <rakesh9901182@gmail.com>
2022-09-20 19:00:32 +0530

    third commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,41 +1,40 @@
 import React from 'react'
 import './Main.css'
-import {useState} from 'react'
+import { useState } from 'react'
 const Main = () => {
   const [text, setText] = useState('');
   return (
     <>
-    <div className='layout'>
-    <div className='main'>
-    <h1>Ceaser Cipher</h1>
-    <div className='container'>
-    <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" className="form-label">Enter the Text</label>
-    <input type="text" className="form-control" id="exampleInputEmail1"  />
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Enter number of Shift </label>
-    <input type="number" className="form-control" id="exampleInputPassword1"/>
-  </div>
-  
-  <button type="submit" className="btn btn-primary" >Submit</button>
+      <div className='layout'>
+        <div className='main'>
+          <h1>Ceaser Cipher</h1>
+          <div className='container'>
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" className="form-label">Enter the Text</label>
+                <input type="text" className="form-control" id="exampleInputEmail1" />
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" className="form-label">Enter number of Shift </label>
+                <input type="number" className="form-control" id="exampleInputPassword1" />
+              </div>
+
+              <button type="submit" className="btn btn-primary" >Submit</button>
 
 
-  <br/>
-  <br/>
-  <be/>
-  <br/>
+              <br />
+              <br />
 
-  <label for="exampleInputPassword1" className="form-label">Generated Text</label>
 
-  <textarea className="form-control" value={text}  >{text}</textarea>
+              <label for="exampleInputPassword1" className="form-label">Generated Text</label>
 
-</form>
+              <textarea className="form-control" value={text}  >{text}</textarea>
 
-    </div>
-    </div>
-    </div>
+            </form>
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
